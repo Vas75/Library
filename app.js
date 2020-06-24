cd78244e835e99f5ac87321618172f2ab76d4ec9
Phillip Vasil Phillipov <kungy1975@gmail.com>
2020-06-24 16:28:36 -0500

    html added for modal/form that will let user add books. Simple styling added to better see progress of project. Functions for looping though array of books, creating html for each book object, and rendering books to browser added.

--- app.js
+++ app.js
@@ -1,16 +1,69 @@
-function Book(title, author, pages, finished) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.finished = finished;
+const bookContainer = document.querySelector("#book-container");
+
+const books = [
+  { author: "J.K. Rowlings", title: "Harry Potter", pages: 500, read: true },
+  { author: "Tolkien", title: "Lord of the Rings", pages: 1000, read: false },
+]; //later this will initialize with contents of local storage
+
+getBooks();
+
+// function Book(title, author, pages, finished) {
+//   this.title = title;
+//   this.author = author;
+//   this.pages = pages;
+//   this.finished = finished;
+// }
+
+// Book.prototype.info = function () {
+//   const readOrNot = this.finished ? "already read" : "not read yet";
+
+//   return `${this.title} by ${this.author}, ${this.pages} pages, ${readOrNot}.`;
+// };
+
+//Page loads, loop gets each book and passes obj to render(),
+function getBooks() {
+  books.forEach((book, index) => {
+    render(book, index);
+  });
 }
 
-Book.prototype.info = function () {
-  const readOrNot = this.finished ? "already read" : "not read yet";
+//render() will create the html using the props of each object and add the book card to a container elem
 
-  return `${this.title} by ${this.author}, ${this.pages} pages, ${readOrNot}.`;
-};
+function render(book, index) {
+  const div = document.createElement("div");
+  div.classList.add("book");
+  div.setAttribute("data-index", index);
 
-const aBook = new Book("hustler", "larry king", 40, true);
+  div.innerHTML = createHTML(book);
 
-console.log(aBook);
+  bookContainer.appendChild(div);
+}
+//helper to create html for the book div,
+function createHTML(book) {
+  const isRead = book.read
+    ? "you have read this book."
+    : "you have not read this book yet.";
+
+  return `<div class="book-btns">
+    <button class="delete">X</button>
+    <button class="readBtn">read</button>
+   </div>
+  <p class="book-info">
+    ${book.title} by ${book.author}, ${book.pages} pages long, ${isRead}
+  </p>`;
+}
+
+//-each card elem will need data attribute w index of its book obj.
+//-each card will have delete btn that will remove obj from array, resave, and render again
+//
+
+//
+
+//
+
+//eventlisteners
+bookContainer.addEventListener("click", (e) => {
+  if (e.target.tagName === "BUTTON") {
+    const index = e.target.parentNode.parentNode.dataset.index;
+  }
+});
